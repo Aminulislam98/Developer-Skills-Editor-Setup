275a5dbb542a8bcc7896056736ac98aa3dd9f890
Aminul islam <aminurrohman98@gmail.com>
2026-06-20 18:07:45 +0100

    Practiced how to get max number from an array

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 
 const calcAge = birthYear => 2026 - birthYear;
 console.log(calcAge(2003));
+
+const number = [2, 3, 4, 5, 6, 7, 8, 89, 1];
+let maxNum = number[0];
+for (let i = 0; i < number.length; i++) {
+  if (number[i] > maxNum) {
+    maxNum = number[i];
+  }
+}
+console.log(`Max number is : ${maxNum}`);
