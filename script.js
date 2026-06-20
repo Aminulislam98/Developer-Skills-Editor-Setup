086b29973f01d783c1862f4fd4c7e7f12100db2e
Aminul islam <aminurrohman98@gmail.com>
2026-06-20 18:14:38 +0100

    applied ternary operator instead of if else condition while it made my code less

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ const number = [2, 3, 4, 5, 6, 7, 8, 89, 1];
 let max = number[0];
 let min = number[0];
 for (let i = 0; i < number.length; i++) {
-  if (number[i] > max) {
-    max = number[i];
-  } else if (number[i] < min) {
-    min = number[i];
-  }
+  //   if (number[i] > max) {
+  //     max = number[i];
+  //   } else if (number[i] < min) {
+  //     min = number[i];
+  //   }
+
+  number[i] > max ? (max = number[i]) : (min = number[i]);
 }
 console.log(`Max number is : ${max}`);
 console.log(`Min number is : ${min}`);
